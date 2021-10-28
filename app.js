172d9b88d54f8372ca05330d5fb0d8b0e633e69c
[roxsneilyd-pinto] <[andreinapm.16@gmail.com]>
2021-10-27 21:41:56 -0500

    Moving routes

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 //const {v1: uuidv1} = require('uuid');
 const app = require('./config/server.config')
-const routes = require('./config/routes.config')
 const port = 3001
 const PATH = '/'
-
-app.use(PATH, routes)
 
 app.listen(port,() => {
     console.log('SERVER ON')
--- config/server.config.js
+++ config/server.config.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const helmet = require('helmet');
+const routes = require('./routes.config')
 
 app.use(helmet())
 app.use(express.urlencoded({extended: true }))
